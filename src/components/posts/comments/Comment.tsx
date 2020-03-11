6b1496f737563a07b0ad046e731caef503b4edcc
Justin Schnurer <JSchnurer@slb.com>
2020-03-11 11:31:09 -0400

    added link on user id

--- src/components/posts/comments/Comment.tsx
+++ src/components/posts/comments/Comment.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./Comment.scoped.css";
 import FormattedDateString from "../../misc/FormattedDateString";
 import { PostComment } from "../../../store/comment/types";
+import { Link } from "react-router-dom";
 
 const Comment: React.FunctionComponent<PostComment> = (comment) => {
   return <div className="holder">
     <div className="speech-bubble">{comment.body}</div>
     <label>
-      {comment.userId}
+      <Link to={`/users/${comment.userId}`}>{comment.userId}</Link>
       {comment.time &&
         <span>, <FormattedDateString date={comment.time} /></span>
       }
