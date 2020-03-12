88992fd73e9716eedcaafbc1bcc874e3faaf1313
Justin Schnurer <JSchnurer@slb.com>
2020-03-12 10:29:51 -0400

    updated welcome

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -5,12 +5,13 @@ const Welcome: React.FC = () =>
   <>
     <h2>Welcome</h2>
     <p>
-      This application is built using React and uses Redux with Redux-Sagas.
-      It also uses craco-scoped-css for component-scoped styling. React-router-dom
-      is used for client-side navigation.
+      This application is built using <a href="https://reactjs.org/">React</a> and uses <a href="https://redux.js.org/">Redux</a> with <a href="https://redux-saga.js.org/">Redux-Saga</a>. It also uses <a href="https://www.npmjs.com/package/craco-plugin-scoped-css">craco-scoped-css</a> for component-scoped styling. <a href="https://reacttraining.com/react-router/">React-router-dom</a> is used for client-side navigation. It uses <a href="https://jaredpalmer.com/formik">Formik</a> with <a href="https://github.com/jquense/yup">Yup</a> validation to handle forms.
     </p>
     <p>
-      <Link to="/posts">Post Listing</Link>
+      For a backend API, it uses <a href="my-json-server.typicode.com/">My JSON Server</a> for a fake datasource. It doesn't persist PUTs, POSTs, or DELETEs so you won't see any changes if you do those using the site.
+    </p>
+    <p>
+      <Link to="/posts">View Blog Posts &gt;</Link>
     </p>
   </>;
 
