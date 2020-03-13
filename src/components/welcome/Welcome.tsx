ad936336d54a120c9fc7160dec780c65a214b87f
Justin Schnurer <justinschnurer@gmail.com>
2020-03-13 19:49:13 -0400

    updated link

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -8,7 +8,7 @@ const Welcome: React.FC = () =>
       This application is built using <a href="https://reactjs.org/">React</a> and uses <a href="https://redux.js.org/">Redux</a> with <a href="https://redux-saga.js.org/">Redux-Saga</a>. It also uses <a href="https://www.npmjs.com/package/craco-plugin-scoped-css">craco-scoped-css</a> for component-scoped styling. <a href="https://reacttraining.com/react-router/">React-router-dom</a> is used for client-side navigation. It uses <a href="https://jaredpalmer.com/formik">Formik</a> with <a href="https://github.com/jquense/yup">Yup</a> validation to handle forms.
     </p>
     <p>
-      For a backend API, it uses <a href="my-json-server.typicode.com/">My JSON Server</a> for a fake datasource. It doesn't persist PUTs, POSTs, or DELETEs so any changes made won't persist if you reload the page.
+      For a backend API, it uses <a href="https://my-json-server.typicode.com/">My JSON Server</a> for a fake datasource. It doesn't persist PUTs, POSTs, or DELETEs so any changes made won't persist if you reload the page.
     </p>
     <p>
       <Link to="/posts">View Blog Posts &gt;</Link>
